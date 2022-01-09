ef284afa449b0863e2c8a13843c2fd5bd86c184c
xsh <xsh2668028614@qq.com>
2022-01-09 22:56:07 +0800

    初始化项目

---
+++ src/main/java/com/xsh/emos/wx/EmosWxApiApplication.java
@@ -0,0 +1,13 @@
+package com.xsh.emos.wx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmosWxApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmosWxApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xsh/emos/wx/EmosWxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xsh.emos.wx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmosWxApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
